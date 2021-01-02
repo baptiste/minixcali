2b1273793b662dcd64c0ca6717d740b23e712e05
baptiste auguie <baptiste.auguie@gmail.com>
2021-01-02 17:28:25 +1300

    file not found

---
+++ vignettes/FlameTree.R
@@ -0,0 +1,11 @@
+## ----setup, echo=FALSE, message=FALSE,warning=FALSE,out.width='100%'----------
+library(minixcali)
+library(purrr)
+library(knitr)
+opts_chunk$set(message = FALSE, 
+               warning=FALSE,
+               fig.width = 7,
+               fig.height = 3, 
+               out.width='100%')
+
+
